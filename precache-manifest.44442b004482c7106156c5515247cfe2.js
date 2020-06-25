cc643d9fecaa8debe4bf1d70e1ea318ad2f06354
hamzaabamboo <hamzaabamboo@users.noreply.github.com>
2020-06-25 04:46:44 +0000

    deploy: 780434236eddb99a6d5711e17083ac5c77c3a7ee

---
+++ precache-manifest.44442b004482c7106156c5515247cfe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02c64d82bda0e1e33ad22eb1999c564",
+    "url": "/covid-trainer/index.html"
+  },
+  {
+    "revision": "8d3e006e7054dff01f4a",
+    "url": "/covid-trainer/static/css/main.c2420030.chunk.css"
+  },
+  {
+    "revision": "f7caeb285c74e5988eea",
+    "url": "/covid-trainer/static/js/2.9c60fa6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-trainer/static/js/2.9c60fa6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3e006e7054dff01f4a",
+    "url": "/covid-trainer/static/js/main.f62ba710.chunk.js"
+  },
+  {
+    "revision": "e480e3101d71350c8d41",
+    "url": "/covid-trainer/static/js/runtime-main.90bbcacf.js"
+  }
+]);
