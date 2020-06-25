51a4a6cc99795a0fa2f902c38d5cb14271f25bae
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2020-06-25 10:45:23 +0700

    :rotating_light:

--- web/src/AppRouter.tsx
+++ web/src/AppRouter.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Home } from "./Home";
-import { BadComponent } from "./BadComponent";
 
 export const AppRouter: React.FC<{}> = () => {
   return (
